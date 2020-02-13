2fa240d3e8a56b2677f6dba5d77a2b10c52ee433
Emad Elshafie <emad_moh@outlook.com>
2020-02-13 23:23:33 +0200

    Added userlevels

--- Api/AuthInterface.php
+++ Api/AuthInterface.php
@@ -6,7 +6,9 @@ namespace Emadello\Auth\Api;
  * interface
  */
 interface AuthInterface
-{  
+{
   CONST USERS_TABLE = '_users';
   CONST ACCESSTOKENS_TABLE = '_access_tokens';
+  CONST ADMIN_USERLEVEL = 9;
+  CONST MOD_USERLEVEL = 8;
 }
