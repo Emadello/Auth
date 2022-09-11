9a3b02dfed547e0b4d911b6793c3cbff617be105
Emad Elshafie <emad_moh@outlook.com>
2022-09-11 11:49:45 +0200

    Fixed PHP notice in installation

--- src/Emadello/Install/Install.php
+++ src/Emadello/Install/Install.php
@@ -7,7 +7,8 @@ use \Emadello\Db;
 use \DevCoder\DotEnv;
 use \Composer\Factory;
 
-class Install implements AuthInterface {
+class Install implements AuthInterface
+{
 
   protected $db;
   protected $getData;
@@ -15,7 +16,8 @@ class Install implements AuthInterface {
   protected $installed = true;
   protected $envFile = '/.env';
 
-  public function __construct() {
+  public function __construct()
+  {
     $this->checkEnvFile();
     $this->db = new Db();
     $this->getData = $_GET;
@@ -26,16 +28,18 @@ class Install implements AuthInterface {
     $this->checkIfInstalled();
   }
 
-  protected function checkEnvFile() {
+  protected function checkEnvFile()
+  {
     $reflection = new \ReflectionClass(\Composer\Autoload\ClassLoader::class);
     $this->projectPath = dirname($reflection->getFileName(), 3);
-    if (!is_file($this->projectPath.'/'.$this->envFile)) {
+    if (!is_file($this->projectPath . '/' . $this->envFile)) {
       echo '<br /><br /><br /><center>Missing env file</center>';
       exit();
     }
   }
 
-  public function checkIfInstalled() {
+  public function checkIfInstalled()
+  {
     if (!$this->tableExists(AuthInterface::LOGINATTEMPTS_TABLE)) $this->forceInstall();
     if (!$this->tableExists(AuthInterface::ACCESSTOKENS_TABLE)) $this->forceInstall();
     if (!$this->tableExists(AuthInterface::USERS_TABLE)) $this->forceInstall();
@@ -43,40 +47,42 @@ class Install implements AuthInterface {
     if (!$this->adminExists()) $this->forceInstall();
   }
 
-  public function adminExists() {
-    $chk = $this->db->con()->query("SELECT user_id FROM ".AuthInterface::USERS_TABLE." WHERE userlevel = ".AuthInterface::ADMIN_USERLEVEL);
+  public function adminExists()
+  {
+    $chk = $this->db->con()->query("SELECT user_id FROM " . AuthInterface::USERS_TABLE . " WHERE userlevel = " . AuthInterface::ADMIN_USERLEVEL);
     if ($chk->rowCount() > 0) return true;
 
     return false;
   }
 
-  public function forceInstall() {
+  public function forceInstall()
+  {
 
     $output = '<center>
     <br /><b>ERROR - One or more of the main tables are missing<br /><br />
-    <a href="'.basename($_SERVER['PHP_SELF']).'?module=Auth&forceInstall=1">Click here to re-install system users</a></b>
+    <a href="' . basename($_SERVER['PHP_SELF']) . '?module=Auth&forceInstall=1">Click here to re-install system users</a></b>
     </center>';
 
     echo $output;
     exit();
-
   }
 
-  public function tableExists($table) {
+  public function tableExists($table)
+  {
 
     try {
 
       $chk = $this->db->con()->query("SHOW TABLES LIKE '$table'");
       if ($chk->rowCount() > 0) return true;
-
-    } catch (PDOException $e) {
+    } catch (\PDOException $e) {
       return false;
     }
 
     return false;
   }
 
-  public function beginInstall() {
+  public function beginInstall()
+  {
 
     $output = '<center><br /><br />';
     try {
@@ -86,26 +92,26 @@ class Install implements AuthInterface {
       if (!$this->tableExists(AuthInterface::LOGINATTEMPTS_TABLE)) {
         // login attempts table does not exists
         $output .= 'Installing _login_attempts table ... ';
-        $this->db->con()->query(\file_get_contents(__DIR__.'/_login_attempts.sql'));
+        $this->db->con()->query(\file_get_contents(__DIR__ . '/_login_attempts.sql'));
         $output .= 'Done<br />';
       }
       if (!$this->tableExists(AuthInterface::ACCESSTOKENS_TABLE)) {
         // login attempts table does not exists
         $output .= 'Installing _users_tokens table ... ';
-        $this->db->con()->query(\file_get_contents(__DIR__.'/_users_tokens.sql'));
+        $this->db->con()->query(\file_get_contents(__DIR__ . '/_users_tokens.sql'));
         $output .= 'Done<br />';
       }
       if (!$this->tableExists(AuthInterface::USERS_TABLE)) {
         // users table does not exists
         $adminUserInstall = true;
         $output .= 'Installing _users table ... ';
-        $this->db->con()->query(\file_get_contents(__DIR__.'/_users.sql'));
+        $this->db->con()->query(\file_get_contents(__DIR__ . '/_users.sql'));
         $output .= 'Done<br />';
       }
       if (!$this->tableExists(AuthInterface::PERMS_TABLE)) {
         // permissions table does not exists
         $output .= 'Installing _users_perms table ... ';
-        $this->db->con()->query(\file_get_contents(__DIR__.'/_users_perms.sql'));
+        $this->db->con()->query(\file_get_contents(__DIR__ . '/_users_perms.sql'));
         $output .= 'Done<br />';
       }
 
@@ -114,23 +120,22 @@ class Install implements AuthInterface {
       if (!$adminUserInstall) $output .= "<br /><br /><b>Successfully Installed Tables</b><br /><br /><a href=\"index.php\">Run Web Application</a>";
       else $output .= $this->drawAdminCredsForm();
       $this->db->con()->commit();
-    } catch (PDOException $e) {
+    } catch (\PDOException $e) {
 
-      $output .= "<b>ERROR Installing tables: ".$e->getMessage()."</b><br /><br /><a href=\"index.php\">Back</a>";
+      $output .= "<b>ERROR Installing tables: " . $e->getMessage() . "</b><br /><br /><a href=\"index.php\">Back</a>";
       $this->db->con()->rollBack();
-
     }
     $output .= '</center>';
     echo $output;
     exit();
-
   }
 
-  public function drawAdminCredsForm() {
+  public function drawAdminCredsForm()
+  {
     $output = '<form method="post">
     <h2>Admin Credentials</h2><br />
     <b>Email</b><br />
-    <input type="email" name="email" value="'.isset($this->postData['email']) ? $this->postData['email'] : ''.'" style="padding:5px; width: 300px" /><br /><br />
+    <input type="email" name="email" value="' . ((isset($this->postData) && isset($this->postData['email'])) ? $this->postData['email'] : '') . '" style="padding:5px; width: 300px" /><br /><br />
     <b>Password</b><br />
     <input type="password" name="password" style="padding:5px; width: 300px" /><br /><br />
     <b>Confirm Password</b><br />
@@ -140,7 +145,8 @@ class Install implements AuthInterface {
     return $output;
   }
 
-  public function installAdminUser() {
+  public function installAdminUser()
+  {
 
     $success = true;
     $error = '';
@@ -163,7 +169,7 @@ class Install implements AuthInterface {
     if ($success) {
 
       // Install admin
-      $sql = $this->db->con()->prepare("INSERT INTO ".AuthInterface::USERS_TABLE." VALUES (
+      $sql = $this->db->con()->prepare("INSERT INTO " . AuthInterface::USERS_TABLE . " VALUES (
         ?,
         ?,
         ?,
@@ -178,7 +184,7 @@ class Install implements AuthInterface {
       exit();
     } else {
       echo '<center>
-      <br /><br />'.$error.'<br /><br />'.$this->drawAdminCredsForm().'
+      <br /><br />' . $error . '<br /><br />' . $this->drawAdminCredsForm() . '
       </center>';
       exit();
     }
